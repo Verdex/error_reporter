4b804df92b3f8816f93d863702650fee2d5f4efd
David L. Inman <davidlevere@gmail.com>
2020-08-22 00:01:41 -0400

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 
-pub fn report( error_message : &str, input : &str, start : usize, end : usize ) -> String {
+pub fn report( input : &str, start : usize, end : usize ) -> String {
     let len = input.len();
     assert!( start < len, "Encountered start index longer than input" );
     assert!( end < len, "Encountered end index longer than input" );
@@ -34,7 +34,7 @@ pub fn report( error_message : &str, input : &str, start : usize, end : usize ) 
     match (before, current, after) {
         (None, Some(c), None) => format!( "{}\n{}\n", c, p ),
         (None, Some(c), Some(a)) => format!( "{}\n{}\n{}\n", c, p, a ),
-        (Some(b) , Some(c), None) => format!( "{}\n{}\n{}\n", b, c, p ),
+        (Some(b), Some(c), None) => format!( "{}\n{}\n{}\n", b, c, p ),
         (Some(b), Some(c), Some(a)) => format!( "{}\n{}\n{}\n{}\n", b, c, p, a ),
         _ => panic!("Enountered start and end outside of input range in error reporter: {} {}", start, end),
     }
@@ -54,11 +54,151 @@ line three
 line four
 "#;
 
-        let output = report( "some error", input, 21, 21 );
+        let output = report( input, 21, 21 );
 
         assert_eq!( output, r#"line one
 line two
 -^
+line three
+"# );
+    }
+
+    #[test]
+    fn should_display_multi_character_error() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 21, 24 );
+
+        assert_eq!( output, r#"line one
+line two
+-^^^^
+line three
+"# );
+    }
+
+    #[test]
+    fn should_display_single_character_error_at_start_of_line() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 20, 20 );
+
+        assert_eq!( output, r#"line one
+line two
+^
+line three
+"# );
+    }
+
+    #[test]
+    fn should_display_single_character_error_at_end_of_line() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 27, 27 );
+
+        assert_eq!( output, r#"line one
+line two
+-------^
+line three
+"# );
+    }
+
+    #[test]
+    fn should_display_full_line_error() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 20, 27 );
+
+        assert_eq!( output, r#"line one
+line two
+^^^^^^^^
+line three
+"# );
+    }
+
+    #[test]
+    fn should_display_single_character_error_at_end_of_file() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four"#;
+
+        let output = report( input, 48, 48 );
+
+        assert_eq!( output, r#"line three
+line four
+--------^
+"# );
+    }
+
+    #[test]
+    fn should_display_single_character_error_at_beginning_of_file() {
+        let input = r#"line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 0, 0 );
+
+        assert_eq!( output, r#"line zero
+^
+line one
+"# );
+    }
+
+    #[test]
+    fn should_display_single_character_for_single_line() {
+        let input = r#"line zero"#;
+
+        let output = report( input, 0, 0 );
+
+        assert_eq!( output, r#"line zero
+^
+"# );
+    }
+
+    #[test]
+    fn should_display_multi_character_a_few_characters_away_from_line_start() {
+        let input = r#"
+line zero
+line one
+line two
+line three
+line four
+"#;
+
+        let output = report( input, 23, 25 );
+
+        assert_eq!( output, r#"line one
+line two
+---^^^
 line three
 "# );
     }
